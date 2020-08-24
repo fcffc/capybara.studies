09f26bb0cf7c201b2dc4d35e32e9df498e3107b2
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-23 20:33:38 -0400

    Testa dropdown

---
+++ spec/dropdown_spec.rb
@@ -0,0 +1,21 @@
+describe 'Caixa de seleçõa' do
+    
+    it 'item especifico simples', :dropdown do # , :dropdown indica uma tag
+        visit 'http://training-wheels-protocol.herokuapp.com/dropdown'
+        select('Loki', from: 'dropdown')
+        sleep 3 # temporario
+    end
+
+    it 'item especifico com o find', :dropdown do
+        visit 'http://training-wheels-protocol.herokuapp.com/dropdown'
+        drop = find('.avenger-list') # quando não tem id
+        drop.find('option', text: 'Scott Lang').select_option
+        sleep 3 # temporario
+    end
+
+    it 'qualquer item', :sample do
+        visit 'http://training-wheels-protocol.herokuapp.com/dropdown'
+        drop = find('.avenger-list') # quando não tem id
+        drop.all('option').sample.select_option # retorna um Array de option e seleciona um de forma randomica
+    end
+end
