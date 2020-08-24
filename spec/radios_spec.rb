b498b694adf83f9867ee935f0568494734d555a9
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-23 21:02:46 -0400

    Adiciona teste de radio

---
+++ spec/radios_spec.rb
@@ -0,0 +1,18 @@
+describe 'Botões de radio', :radio do
+    
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/radios'
+    end
+
+    it 'seleçõa por id' do
+        choose('cap') # quando possui id use choose
+    end
+
+    it 'seleçõa por find e css selector' do
+        find('input[value=guardians]').click
+    end
+
+    after(:each) do
+        sleep 3
+    end
+end
