9b65f61b91596704c77b21c1f22a73d66f825c59
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-23 20:52:34 -0400

    Adiciona teste de checkbox

---
+++ spec/checkboxes_spec.rb
@@ -0,0 +1,28 @@
+describe 'Caixas de seleção', :checkbox do
+
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/checkboxes'
+    end
+    
+    it 'marcando uma opçõa' do
+        check('thor')
+    end
+
+    it 'desmarcar uma opção' do
+        uncheck('antman')
+    end
+
+    it 'marcando com find set true' do
+        find('input[value=cap]').set(true)
+    end
+
+    it 'desmarcando com find set false' do
+        find('input[value=guardians]').set(false)
+    end
+
+    after(:each) do
+        sleep 3
+    end
+end
+
+# rspec --tag @checkbox -fd -> para rodar tag
