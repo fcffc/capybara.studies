f41c187f38a60bf2349655e2e6f660d01ae37743
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-24 14:22:37 -0400

    Adiciona teste com teclado

---
+++ spec/key_spec.rb
@@ -0,0 +1,28 @@
+describe 'Teclado', :key do
+    
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/key_presses'
+    end
+
+    it 'enviando teclas' do
+        # Forma 1
+        # find('#campo-id').send_keys :tab
+        # expect(page).to have_content 'You entered: TAB'
+
+        # Forma 2
+        teclas = %i[tab escape space enter shift control alt] # %i para criar array de simbolos
+        teclas.each do |t|
+            find('#campo-id').send_keys t
+            expect(page).to have_content 'You entered: ' + t.to_s.upcase
+        end
+    end
+
+    it 'enviando letras' do
+        letras = %w[a s d f g h j k l]  # %w para criar array de strings
+        letras.each do |l|
+            find('#campo-id').send_keys l
+            expect(page).to have_content 'You entered: ' + l.to_s.upcase
+            sleep 1
+        end
+    end
+end
