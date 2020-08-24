96939be1b5309d8af4aa9917cc79484310b173f7
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-23 23:49:08 -0400

    Adiciona teste de mouse hover

---
+++ spec/hovers_spec.rb
@@ -0,0 +1,28 @@
+describe 'Mouse Houver', :hovers do
+    
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/hovers'
+    end
+
+    it 'quando passo o mouse sobre o Blade' do
+        find('img[alt=Blade]').hover
+
+        expect(page).to have_content 'Nome: Blade'
+    end
+
+    it 'quando passo o mouse sobre o Pantera Negra' do
+        find('img[alt="Pantera Negra"]').hover
+
+        expect(page).to have_content 'Nome: Pantera Negra'
+    end
+
+    it 'quando passo o mouse sobre o Homem Aranha' do
+        find('img[alt="Homem Aranha"]').hover
+
+        expect(page).to have_content 'Nome: Homem Aranha'
+    end
+
+    after(:each) do
+        sleep 0.5 # temporário
+    end
+end
