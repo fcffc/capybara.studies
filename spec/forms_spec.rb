a147aa2f89a9f798f181753613bee963187562b7
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-23 20:08:50 -0400

    Adiciona novos cenários

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -15,4 +15,30 @@ describe 'Forms' do
 
         expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
     end
+
+    it 'senha incorreta' do
+        visit 'http://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'userId', with: 'stark'
+        fill_in 'password', with: 'jarvis'
+
+        click_button 'Login'
+
+        expect(find('.flash.error').visible?).to be true
+        expect(find('.flash.error').text).to include 'Senha é invalida!'
+
+    end
+
+    it 'usuário não cadastrado' do
+        visit 'http://training-wheels-protocol.herokuapp.com/login'
+
+        fill_in 'userId', with: 'fernanda'
+        fill_in 'password', with: 'jarvis!'
+
+        click_button 'Login'
+
+        expect(find('.flash.error').visible?).to be true
+        expect(find('.flash.error').text).to include 'O usuário informado não está cadastrado!'
+        
+    end
 end
