37678b05d9b80161512ce28772466fa234946a79
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-24 00:20:07 -0400

    Adiciona teste com drag and drop

---
+++ spec/drag_drop_spec.rb
@@ -0,0 +1,24 @@
+describe 'Drag and Drop', :drop do
+
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/drag_and_drop'
+    end
+    
+    it 'homem aranha pertence ao time do stark' do
+        
+        # Forma 1
+        # stark = find('.team-stark .column')
+        # cap = find('.team-cap .column')
+        # spiderman = find('img[alt$=Aranha]')
+        # spiderman.drag_to stark
+
+        # expect(stark).to have_css 'img[alt$=Aranha]'
+        # expect(cap).not_to have_css 'img[alt$=Aranha]'
+
+        # Forma 2
+        find('img[alt$=Aranha]').drag_to find('.team-stark .column')
+        expect(find('.team-stark .column')).to have_css 'img[alt$=Aranha]' # verifica se a img contem em stark
+        expect(find('.team-cap .column')).not_to have_css 'img[alt$=Aranha]' # verifica se a img Não contem em cap
+
+    end
+end
