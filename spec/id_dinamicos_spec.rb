48433910ce89f0f66775f11fb595a243b1b43aaa
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-24 20:42:28 -0400

    Implementa regex para teste com id dinamico

---
+++ spec/id_dinamicos_spec.rb
@@ -0,0 +1,15 @@
+describe 'IDs dinâmicos', :ids_dinamicos do
+    
+    before(:each) do
+        visit 'http://training-wheels-protocol.herokuapp.com/access'
+    end
+
+    it 'cadastro' do
+        find('input[id$=UsernameInput]').set 'fernanda'
+        find('input[id^=PasswordInput]').set 'fernanda'
+        find('a[id*=GetStartedButton]').click
+
+        expect(page).to have_content 'Dados enviados. Aguarde aprovação do seu cadastro!'
+        sleep 3
+    end
+end
