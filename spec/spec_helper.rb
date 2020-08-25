87972ecb60bf18fabe9187ade574dfc9bc0d4607
Fernanda Ferreira <fernanda.ferreira@nuvem.net>
2020-08-24 23:12:34 -0400

    Altera execução dos testes para headless

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,12 +12,24 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.before(:example) do
+    page.current_window.resize_to(1920, 1080)
+  end
+
+  config.after(:example) do |e|
+    nome = e.description.gsub(/[^A-Za-z0-9 ]/, '').tr(' ', '_')
+    # if e.exeption condição para obter evidência somente quando o teste falha
+    # para todos os cenários, tire o if
+    page.save_screenshot('log/' + nome + '.png')  if e.exception
+  end
+
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.include Capybara::DSL # Inclui recursos do Capybara
 end
 
 Capybara.configure do |config|
   # config.default_driver = :selenium_chrome
-  config.default_driver = :selenium #firefox
+  config.default_driver = :selenium_chrome_headless
+  # config.default_driver = :selenium #firefox
   config.default_max_wait_time = 5
 end
